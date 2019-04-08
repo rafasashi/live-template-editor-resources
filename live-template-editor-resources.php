85268ee2c7c6aff5edaea28ab49c58c457c02fdd
Rafasashi\Raphael <raphael.dartigues@gmail.com>
2019-04-08 09:39:41 +0530

    first commit

---
+++ live-template-editor-resources.php
@@ -0,0 +1,21 @@
+<?php
+/*
+ * Plugin Name: Live Template Editor Resources
+ * Version: 1.1.1
+ * Plugin URI: https://github.com/rafasashi
+ * Description: Resources for Live Template Editor server.
+ * Author: Rafasashi
+ * Author URI: https://github.com/rafasashi
+ * Requires at least: 4.6
+ * Tested up to: 4.7
+ *
+ * Text Domain: ltple-client
+ * Domain Path: /lang/
+ *
+ * GitHub Plugin URI: rafasashi/live-template-editor-resources
+ * GitHub Branch:     master
+ *
+ * @package WordPress
+ * @author Rafasashi
+ * @since 1.0.0
+ */
